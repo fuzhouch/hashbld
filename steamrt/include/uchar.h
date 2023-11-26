37d845af2decd7c6a524cc3db1d773eb3c0a6431
fuzhou.chen <fuzhouch@gmail.com>
2023-11-26 14:19:14 +0800

    [BLD] Avoid naming conflict of char16_t and char32_t btw C and C++.

--- steamrt/include/uchar.h
+++ steamrt/include/uchar.h
@@ -1,17 +1,12 @@
 #ifndef _STEAMRT_UCHAR_H_
 #define _STEAMRT_UCHAR_H_
 
-#ifdef __cplusplus
-extern "C" {
+#ifndef __cplusplus
 
 #include <stdint.h>
-
-#ifndef __USE_ISOCXX11
 
 typedef uint16_t char16_t;
 typedef uint32_t char32_t;
 
-#endif
-} /* extern "C" */
 #endif /* __cplusplus */
 #endif /* _STREAMRT_UCHAR_H_ */
