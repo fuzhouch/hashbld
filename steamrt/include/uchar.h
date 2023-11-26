4bfec67f300b01debb1efd0579ba35f2445110c5
fuzhou.chen <fuzhouch@gmail.com>
2023-11-26 11:42:48 +0800

    [FIX] Avoid type definition conflict on uchar.h btw C and C++.

--- steamrt/include/uchar.h
+++ steamrt/include/uchar.h
@@ -6,8 +6,12 @@ extern "C" {
 
 #include <stdint.h>
 
+#ifndef __USE_ISOCXX11
+
 typedef uint16_t char16_t;
 typedef uint32_t char32_t;
-}
+
+#endif
+} /* extern "C" */
 #endif /* __cplusplus */
 #endif /* _STREAMRT_UCHAR_H_ */
