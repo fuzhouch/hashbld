5d74be3adbd90a3df4618fe72bf26d9c97f87db9
fuzhou.chen <fuzhouch@gmail.com>
2023-11-28 00:10:13 +0800

    [FIX] Use option -std=gnu99 instead of -std=c99
    We use clang to build code, while clang does not accept c99 on C++ code.

--- xmake.lua
+++ xmake.lua
@@ -15,7 +15,7 @@ toolchain("steamrt")
     set_toolset("mxx", "clang", "clang++")
     set_toolset("as", "clang")
 
-    add_cxflags("-std=c99")
+    add_cxflags("-std=gnu99")
     add_ldflags("-pthread")
     add_includedirs("/usr/include", "/usr/lib/llvm-3.8/lib/clang/3.8.0/include/")
 toolchain_end()
