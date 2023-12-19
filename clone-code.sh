850d3b5f7d49f832419459f14ab16842f6d9f296
fuzhou.chen <fuzhouch@gmail.com>
2023-12-20 02:08:12 +0800

    [FIX] Remove unused patching echo messages.

--- clone-code.sh
+++ clone-code.sh
@@ -11,12 +11,6 @@ else
     git clone https://github.com/HaxeFoundation/hashlink hashlink
     echo Cloning done.
 fi
-echo ===============================================================
-echo Patching openal-static-build patch ...
-cd hashlink
-cd ..
-echo Patching done.
-echo ===============================================================
 # 
 # Hashlink master branch must work with Heaps master. Using Hashlink
 # from master branch and Heaps with Haxelib can causes ABI mangling
@@ -31,7 +25,7 @@ echo Hashlink code checked out. Please build with Xmake with steps below.
 echo 
 echo     xmake config -p windows -a x64 -m {debug or release}
 echo     or
-echo     xmake config -p {linux or macosx} -a x86_64 -m { debug or release}
+echo     xmake config -p {linux or macosx} -a x86_64 -m {debug or release}
 echo     xmake build
 echo     xmake install -o package64
 echo ===============================================================
